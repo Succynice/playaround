d00bb7ea7b857319a1d606c4c32f33d41203082f
succynice <succynice@gmail.com>
2023-09-06 14:28:00 +0100

    just save this next app here

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+
+      {/* <MatrixBackground /> */}
+    </nav>
+  )
+}
